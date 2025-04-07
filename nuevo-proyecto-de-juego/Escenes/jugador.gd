6e47f35fca8e99b6342d84aaa5a9f58e678fda5c
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-04-07 23:08:05 +0200

    Primer

---
+++ nuevo-proyecto-de-juego/Escenes/jugador.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var gravetat := Vector2.DOWN * 988
+var SALTS := -400
+var velocitat = 0
+@onready var animacio_jugador = $AnimatedSprite2D
+
+func _process(delta: float) -> void:
+	velocity.x = velocitat 
+	animacio()
+	
+	if Input.is_action_just_pressed("SALTAR"):
+		velocity.y = SALTS
+	velocity += gravetat * delta
+	move_and_slide()
+	
+func animacio():
+	if velocity.y < 0:
+		animacio_jugador.play("Volar")
+	else:
+		animacio_jugador.play("default")
---
+++ nuevo-proyecto-de-juego/Escenes/main.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@onready var flappy = $CharacterBody2D
+@onready var nous_murs = $Nous_Murs
+@export var escena_mur = preload("res://Escenes/mur.tscn")
+
+var velocitat_scroll = 200
+var distancia_entre_murs = 300
+var num_murs_inicials = 5
+
+func _ready():
+	print("ready")
+
+func _process(delta):
+	for mur in nous_murs.get_children():
+		mur.position.x -= velocitat_scroll * delta
+		if mur.position.x + 600 < flappy.position.x:
+			mur.queue_free()
+			crea_mur()
+
+func crea_mur():
+	var mur_mes_dreta_x = 0
+	for mur in nous_murs.get_children():
+		if mur.position.x > mur_mes_dreta_x:
+			mur_mes_dreta_x = mur.position.x
+
+	# Crea un mur a l'extrem dret
+	var nova_posicio_x = mur_mes_dreta_x + distancia_entre_murs
+	var mur = escena_mur.instantiate()
+	mur.position = Vector2(nova_posicio_x, randf_range(200, 400))  
+	nous_murs.add_child(mur)
+	
